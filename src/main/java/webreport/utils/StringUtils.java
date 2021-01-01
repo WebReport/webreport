f1fc3509c2f6991fb1f64b9bbfa012ec7121c68f
Cold-Dev <cold.roblox.dev@gmail.com>
2021-01-01 23:49:34 +0000

    Almost forgot to add the licensing message to StringUtils.java, oops

--- src/main/java/webreport/utils/StringUtils.java
+++ src/main/java/webreport/utils/StringUtils.java
@@ -1,7 +1,24 @@
 package webreport.utils;
 
-import org.bukkit.ChatColor;
+/*
+        WebReport - A modern solution for handling in-game reports on the web.
+        Copyright (C) 2021  Cold-Dev
 
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU Affero General Public License as published
+        by the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU Affero General Public License for more details.
+
+        You should have received a copy of the GNU Affero General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import org.bukkit.ChatColor;
 import java.util.Random;
 
 public class StringUtils {
